1f010b412c9a7a6da74b502e1988b5174bbee31d
Francesca Padovani <f.padovani@rug.nl>
2025-08-06 14:19:50 +0200

    update file

--- download_hf.py
+++ download_hf.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 
 snapshot_download(
-    repo_id="fpadovani/communicative-baby-dpo",
-    local_dir="finetuned_models/communicative-baby-dpo",
+    repo_id="fpadovani/communicative-baby-dpo-synthetic",
+    local_dir="finetuned_models/communicative-baby-dpo-synthetic",
     local_dir_use_symlinks=False  # Optional: avoids symlinks, useful for portability
 )
